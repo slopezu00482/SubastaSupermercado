689c05be4043151216b2f932b8fb2e334bde40a2
Slopezu00482 <slopezu00482@gmail.com>
2026-04-10 22:30:40 -0500

    Se actualiza la estrategia de subasta cerrada

---
+++ EstrategiaCerrada.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class EstrategiaCerrada : ISubastaStrategy
+    {
+        public bool ValidarOferta(decimal monto, decimal ofertaActual, decimal precioInicial) => true;
+
+        public Usuario DeterminarGanador(List<Oferta> ofertas) => ofertas.OrderByDescending(o => o.Monto).FirstOrDefault()?.Postulante;
+
+        public bool DebeCerrarPorTiempo(int segundosRestantes, int cantidadOfertas) => segundosRestantes <= 0;
+    }
+}
