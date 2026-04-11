105fd18cec664aeb6d0f480f83d591beccdc8e95
Slopezu00482 <slopezu00482@gmail.com>
2026-04-10 22:16:16 -0500

    Corrige un error en el nobre de las clases y se añaden

---
+++ Ropa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Ropa : Producto
+    {
+        public bool EsLiquidacion { get; set; }
+
+        public override decimal CalcularPrecioFinal(decimal precioSubasta)
+        {
+            return EsLiquidacion ? precioSubasta * 0.80m : precioSubasta;
+        }
+    }
+}
---
+++ RopaFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class RopaFactory : ProductoFactory
+    {
+        public override Producto CrearProducto(string nombre, decimal precio) =>
+        new Ropa { Nombre = nombre, PrecioInicial = precio, EsLiquidacion = true };
+    }
+}
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubastaSupermercado
+{
+    internal class RopaFactory
+    {
+    }
+}
